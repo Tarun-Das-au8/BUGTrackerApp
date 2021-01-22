47ab86e937ef043faf31df88cfd717b5f6716e1d
Tarun Das <imtarun79@gmail.com>
2021-01-22 17:38:05 +0530

    server root file added

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Initialize Middleware
+app.use(express.json());
+
+// Bring in Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/bugs', require('./routes/bugs'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
